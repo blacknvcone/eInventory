7e77e9f7163f2937d8d9371fcc644e99550392bc
Administrator <admin@example.com>
2016-12-28 01:58:30 +0700

    Adding mysql-connector + spring jpa into depedency

--- src/main/java/app/eInvent/eInventModel/Product.java
+++ src/main/java/app/eInvent/eInventModel/Product.java
@@ -1,4 +1,17 @@
 package app.eInvent.eInventModel;
+
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 public class Product {
     private long productId;
     private String productName;
